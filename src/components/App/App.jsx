26b179e80f1bd67b2a1220dc4da658f9fde90039
Simeon Willard <swillard4597@gmail.com>
2021-01-14 13:30:34 -0600

    moved resetItems into app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import ResetButton from '../ResetButton/ResetButton.jsx';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+
+
+
+    const resetItems = (list) => {
+
+        axios.put('/list/reset', {
+            purchased: FALSE
+        }).then(response => {
+            console.log('the purchased items have been reset');
+        }).catch((error) => {
+            console.log('error in reset', error);
+        })
+    } // end reset
+
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
+            <div>
+                <button onClick={resetItems}>Reset</button>
+            </div>
             </main>
         </div>
     );
