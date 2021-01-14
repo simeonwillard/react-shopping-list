52f226f0433d5404170a8e926f0aa58515b72df6
Simeon Willard <swillard4597@gmail.com>
2021-01-14 09:40:00 -0600

    updated database.sql

--- database.sql
+++ database.sql
@@ -1,2 +1,13 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "shopping_list" (
+    "id" SERIAL PRIMARY KEY,
+    "purchased" BOOLEAN DEFAULT FALSE,
+    "name" VARCHAR(80) NOT NULL,
+    "quantity" DECIMAL(4,2) NOT NULL,
+    "unit" VARCHAR(20) 
+);
+
+INSERT INTO "shopping_list" ("name", "quantity", "unit")
+VALUES ('carrots', 3, 'lbs'), ('apples', 5, 'lbs'), ('french bread', 1, 'loaf'), ('pecans', 3, 'cups'), ('milk', 2, 'gallons');
